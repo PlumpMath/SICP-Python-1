9625938c5659e958ba6a7b22ab847629fda93159
Alice <1399789151@qq.com>
2016-07-08 17:18:57 +0800

    chapter1.1

---
+++ chapter1-1.py
@@ -0,0 +1,31 @@
+from operator import add, sub
+def a_plus_abs_b(a, b):
+    """Return a+abs(b), but without calling abs."""
+    if b < 0:
+        op = sub
+    else:
+        op = add
+    return op(a, b)
+
+C = a_plus_abs_b(1,-3)    
+
+
+def if_function(condition, true_result, false_result):
+    """Return true_result if condition is a true value, and false_result otherwise."""
+    if condition == 1:
+        return true_result
+    else:
+        return  false_result
+
+import random
+def c():
+	return random.randint(1,100)
+
+def t():
+	return 1
+
+def f():
+	return 'more than 1'	
+	
+
+print if_function(c(),t(),f())
